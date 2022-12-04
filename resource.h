b35f716479ce3151700ea7a7f698128a8f5dcda5
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2022-12-04 12:27:30 +0700

    переписал код с нуля, но пока оч сыроват

--- resource.h
+++ resource.h
@@ -13,78 +13,6 @@
 #define IDI_SMALL                       108
 #define IDC_TINELIXIRC                  109
 #define IDR_MAINFRAME                   128
-<<<<<<< Updated upstream
-#define IDD_MAINWINDOW                  130
-#define IDD_IRCCHATPAGE                 131
-#define IDR_MAINMENU                    132
-#define IDD_CONNMAN                     134
-#define IDD_EDITTEXT                    136
-#define IDD_SETTINGSDLG                 137
-#define IDD_ABOUTDLG                    139
-#define IDD_STATISTICSDLG               140
-#define IDR_TRAY                        141
-#define IDR_TRAY_NC                     142
-#define IDD_PARSER_S                    143
-#define IDD_INFOMSG                     144
-#define IDD_IRCCHANNELPAGE              145
-#define IDD_MENTIONWND                  146
-#define IDC_IRC_CHAT_TABS               1000
-#define IDC_SOCKMSGS                    1000
-#define IDC_LANGCOMBO1                  1000
-#define IDC_CONNECTION_PROGRESS         1001
-#define IDC_MSGTEXT                     1001
-#define IDC_LANGLABEL                   1001
-#define IDC_STATUSBAR_TEXT              1002
-#define IDC_SENDMSG                     1002
-#define IDC_PROFILES_GROUP              1002
-#define IDC_CHANGE_MSG_FONT             1002
-#define IDC_CONNETCTION_UPTIME          1003
-#define IDC_PROFILELIST                 1003
-#define IDC_ADD_PROFILE_BTN             1004
-#define IDC_APPNAME                     1004
-#define IDC_CONNECT_PROFILE_BTN         1005
-#define IDC_ENTERTEXTEDIT               1005
-#define IDC_TECHINFO                    1005
-#define IDC_RESTARTREQUIRED             1005
-#define IDC_CHANGE_PROFILE_BTN          1006
-#define IDC_ENTERTEXTLABEL              1006
-#define IDC_WINVER_LABEL                1006
-#define IDC_DELETE_PROFILE_BTn          1007
-#define IDC_WINVER                      1007
-#define IDC_TRAFFIC_GROUP               1008
-#define IDC_SENDED_LABEL                1009
-#define IDC_SENDED_LABEL2               1010
-#define IDC_INTERFACE_GROUP             1010
-#define IDC_RECIEVED_LABEL              1011
-#define IDC_FUNCTIONAL_GROUP            1011
-#define IDC_RECIEVED_LABEL2             1012
-#define IDC_APPEARANCE_GROUP            1012
-#define IDC_TOTAL_LABEL                 1013
-#define IDC_TOTAL_LABEL2                1014
-#define IDC_PARSER_GROUP                1014
-#define IDC_SHOW_MSGTIME                1015
-#define IDC_TIMESTAMPS_POS              1016
-#define IDC_INFOMSG_TEXT                1018
-#define IDC_DONT_SHOW                   1019
-#define IDC_WIN32S_EDITION              1020
-#define IDC_WIN32_EDITION               1020
-#define IDC_LICENSE_NOTIFICATION        1021
-#define IDC_COPYRIGHT                   1022
-#define IDC_MEMBERS_LIST                1022
-#define IDC_GITHUB_REPO_LINK            1023
-#define IDC_OPEN                        1024
-#define IDC_SAVE_MSG_HISTORY            1025
-#define IDC_TITLE                       1025
-#define IDC_MINIMIZE_TO_TRAY            1026
-#define IDC_MENTIONED_MSG               1026
-#define IDC_PARSER_LOAD                 1027
-#define IDC_PARSER_SETTINGS             1028
-#define ID_HELP_ABOUT                   32771
-#define ID_VIEW_SETTINGS                32772
-#define ID_FILE_CONNECT                 32773
-#define ID_FILE_QUIT                    32774
-#define ID_FILE_STATISTICS              32775
-=======
 #define IDD_MAINWINDOW                  129
 #define IDD_CONNECTIONDLG               130
 #define IDC_THREADSTABS                 1000
@@ -92,23 +20,15 @@
 #define IDC_MSGEDIT                     1001
 #define ID_SEND                         1002
 #define IDC_STATIC                      -1
->>>>>>> Stashed changes
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-<<<<<<< Updated upstream
-#define _APS_NEXT_RESOURCE_VALUE        147
-#define _APS_NEXT_COMMAND_VALUE         32776
-#define _APS_NEXT_CONTROL_VALUE         1027
-#define _APS_NEXT_SYMED_VALUE           101
-=======
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           110
->>>>>>> Stashed changes
 #endif
 #endif
